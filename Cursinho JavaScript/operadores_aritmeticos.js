63f3a82ad5bb299f2bde37f87115d9cf89997051
natalete <natsalete14@gmail.com>
2023-11-12 19:20:15 -0300

    terceiro commit

---
+++ Cursinho JavaScript/operadores_aritmeticos.js
@@ -0,0 +1,33 @@
+//OPERADORES ARITMÉTRICOS
+
+let primeiroNumero = 20;
+let segundoNumero = 2;
+
+// Adição:     +
+// Subtração:   -
+// multiplicação:   *
+// divisão:        /
+// exponecial:    **
+// resto da divisão:    %
+
+const soma = primeiroNumero + segundoNumero
+
+console.log("Soma =", soma)
+console.log("Subtração =", primeiroNumero  - segundoNumero)
+console.log("Multiplicação =", primeiroNumero * segundoNumero)
+console.log("Divisão =", primeiroNumero / segundoNumero)
+console.log("Exponencial =", primeiroNumero ** segundoNumero)
+console.log("Resto da divisão =",primeiroNumero % segundoNumero)
+
+
+// Precedência de Operadores
+
+console.log((3+7) * 3)
+console.log((10 + 7 +8) / 3)
+
+// Biblioteca Math
+
+console.log(Math.PI)
+console.log(Math.sqrt(16))
+
+// javaScrip faz mdn
---
+++ Cursinho JavaScript/operadores_boolweanos.js
@@ -0,0 +1,36 @@
+//OPERADORES BOOLEANOS
+
+//Igualdade:     == (ou ===)
+//Desigualdade:     != (ou !==)
+//Maior que:         >
+//Maior ou igual:    >=
+//Menor que:         <
+//Menor ou igual:    <=
+
+// dois iguais compara conteúdo já três iguais compara comnteudo e variavel
+const numero = 10
+
+console.log(numero == 10)
+console.log(numero > 20 )
+console.log(10 === '10')
+
+//console.clear() limpa o terminal
+ 
+console.clear()
+
+// - Conjunções lógicas
+
+// AND => &&
+
+ let idade = 26;
+ let tenhoCNH = true;
+
+ const possoDirigir = idade >= 18 && tenhoCNH === true
+
+ console.log("Posso dirigir?", possoDirigir)
+
+ // OR => ||
+
+    idade
+
+ // NOT => !
