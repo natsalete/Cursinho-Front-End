c607ff773b7e976b61004a880f12e3afeae3ff02
natalete <natsalete14@gmail.com>
2023-11-12 18:22:41 -0300

    segundo commit

---
+++ Cursinho JavaScript/variaveis.js
@@ -0,0 +1,33 @@
+// VARIÁVEIS
+
+let idade = 26; //Criando primeira variável
+
+console.log(idade)
+
+// Tipos de variáveis
+idade = 19;     //number
+let numero = 30;     //number
+let altura = 1.54    //number
+let nome = 'Natalia'      //string (texto)
+let  estudando = true     //Booleano (true ou false)
+
+// typeof imprime o tipo de variavel
+
+console.log(numero, typeof numero)
+console.log(altura, typeof altura)
+console.log(nome, typeof nome)
+console.log(estudando, typeof estudando)
+
+//  let outra = undefined Não define o tipo de variavel e indefinido
+
+let outra;
+
+ console.log(outra)
+
+ let nula = null;
+
+ console.log(nula)
+
+ var variavel = 30; //não utilize o var
+
+ const constante = 100;
